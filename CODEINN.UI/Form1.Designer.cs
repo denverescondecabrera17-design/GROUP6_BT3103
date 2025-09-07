fd8b2181ec6dedfc52b57d4d7e3b737e93432821
denverescondecabrera17-design <denverescondecabrera17@gmail.com>
2025-09-07 19:40:42 +0800

    UI LOGIN

--- CODEINN.UI/Form1.Designer.cs
+++ CODEINN.UI/Form1.Designer.cs
@@ -28,12 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            Username = new TextBox();
+            Password = new TextBox();
+            Loginbutton = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // Username
+            // 
+            Username.Font = new Font("Arial Narrow", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Username.Location = new Point(136, 158);
+            Username.Name = "Username";
+            Username.Size = new Size(282, 30);
+            Username.TabIndex = 0;
+            // 
+            // Password
+            // 
+            Password.Font = new Font("Arial Narrow", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Password.Location = new Point(136, 219);
+            Password.Name = "Password";
+            Password.Size = new Size(282, 30);
+            Password.TabIndex = 1;
+            Password.TextChanged += Password_TextChanged;
+            // 
+            // Loginbutton
+            // 
+            Loginbutton.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Loginbutton.Location = new Point(217, 268);
+            Loginbutton.Name = "Loginbutton";
+            Loginbutton.Size = new Size(94, 29);
+            Loginbutton.TabIndex = 2;
+            Loginbutton.Text = "Login";
+            Loginbutton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Narrow", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(36, 161);
+            label1.Name = "label1";
+            label1.Size = new Size(93, 24);
+            label1.TabIndex = 3;
+            label1.Text = "Username:";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial Narrow", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(38, 221);
+            label2.Name = "label2";
+            label2.Size = new Size(92, 24);
+            label2.TabIndex = 4;
+            label2.Text = "Password:";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources._541242678_782352804536515_6233556420554188928_n_removebg_preview;
+            pictureBox1.Location = new Point(358, -18);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(458, 435);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(162, 73);
+            label3.Name = "label3";
+            label3.Size = new Size(214, 44);
+            label3.TabIndex = 6;
+            label3.Text = "WELCOME";
+            label3.Click += label3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(Loginbutton);
+            Controls.Add(Password);
+            Controls.Add(Username);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox Username;
+        private TextBox Password;
+        private Button Loginbutton;
+        private Label label1;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private Label label3;
     }
 }
