677d8bfe8a724f05d6bcdbb948622a8fecf10219
denverescondecabrera17-design <denverescondecabrera17@gmail.com>
2025-09-23 23:24:39 +0800

    done

---
+++ CODEINN.UI/FrontDeskGuestInfo.Designer.cs
@@ -0,0 +1,389 @@
+﻿namespace CODEINN.UI
+{
+    partial class FrontDeskGuestInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            panel1 = new Panel();
+            label4 = new Label();
+            label1 = new Label();
+            bindingSource1 = new BindingSource(components);
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            button1 = new Button();
+            textBox6 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            comboBox1 = new ComboBox();
+            dataGridView1 = new DataGridView();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Gold;
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(label4);
+            panel1.Location = new Point(-9, 1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1323, 63);
+            panel1.TabIndex = 1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(26, 13);
+            label4.Name = "label4";
+            label4.Size = new Size(249, 35);
+            label4.TabIndex = 2;
+            label4.Text = "Guest Information";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(26, 134);
+            label1.Name = "label1";
+            label1.Size = new Size(85, 21);
+            label1.TabIndex = 2;
+            label1.Text = "Guest ID:";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ControlLightLight;
+            textBox1.Font = new Font("Arial", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(29, 75);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(462, 34);
+            textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(26, 175);
+            label3.Name = "label3";
+            label3.Size = new Size(101, 21);
+            label3.TabIndex = 5;
+            label3.Text = "Last Name:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(449, 171);
+            label5.Name = "label5";
+            label5.Size = new Size(102, 21);
+            label5.TabIndex = 6;
+            label5.Text = "First Name:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(927, 174);
+            label6.Name = "label6";
+            label6.Size = new Size(114, 21);
+            label6.TabIndex = 7;
+            label6.Text = "Middle Initial:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(29, 219);
+            label7.Name = "label7";
+            label7.Size = new Size(46, 21);
+            label7.TabIndex = 8;
+            label7.Text = "Age:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.Location = new Point(449, 220);
+            label8.Name = "label8";
+            label8.Size = new Size(95, 21);
+            label8.TabIndex = 9;
+            label8.Text = "Birth Date:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(927, 213);
+            label9.Name = "label9";
+            label9.Size = new Size(75, 21);
+            label9.TabIndex = 10;
+            label9.Text = "Gender:";
+            label9.Click += label9_Click;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label10.Location = new Point(26, 270);
+            label10.Name = "label10";
+            label10.Size = new Size(59, 21);
+            label10.TabIndex = 11;
+            label10.Text = "Email:";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label11.Location = new Point(448, 268);
+            label11.Name = "label11";
+            label11.Size = new Size(135, 21);
+            label11.TabIndex = 12;
+            label11.Text = "Phone Number:";
+            label11.Click += label11_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(139, 131);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(281, 28);
+            textBox2.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(139, 171);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(281, 28);
+            textBox3.TabIndex = 14;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox4.Location = new Point(139, 214);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(79, 28);
+            textBox4.TabIndex = 15;
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox5.Location = new Point(139, 261);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(281, 28);
+            textBox5.TabIndex = 16;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(497, 78);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 17;
+            button1.Text = "Search";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox6.Location = new Point(591, 168);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(289, 28);
+            textBox6.TabIndex = 18;
+            // 
+            // textBox8
+            // 
+            textBox8.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox8.Location = new Point(592, 261);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(288, 28);
+            textBox8.TabIndex = 20;
+            // 
+            // textBox9
+            // 
+            textBox9.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox9.Location = new Point(1047, 168);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(70, 28);
+            textBox9.TabIndex = 21;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dateTimePicker1.Location = new Point(592, 213);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(288, 28);
+            dateTimePicker1.TabIndex = 22;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("Arial Narrow", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(1047, 213);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(70, 30);
+            comboBox1.TabIndex = 23;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(29, 371);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(1259, 313);
+            dataGridView1.TabIndex = 24;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(947, 336);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 25;
+            button2.Text = "Add";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.Location = new Point(1062, 336);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 26;
+            button3.Text = "Save";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.Location = new Point(1176, 336);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 27;
+            button4.Text = "Edit";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button5.Location = new Point(1221, 19);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 29);
+            button5.TabIndex = 28;
+            button5.Text = "Back";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // FrontDeskGuestInfo
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1318, 707);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(dataGridView1);
+            Controls.Add(comboBox1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(textBox9);
+            Controls.Add(textBox8);
+            Controls.Add(textBox6);
+            Controls.Add(button1);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            Name = "FrontDeskGuestInfo";
+            Text = "FrontDeskGuestInfo";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label4;
+        private Label label1;
+        private BindingSource bindingSource1;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Button button1;
+        private TextBox textBox6;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private DateTimePicker dateTimePicker1;
+        private ComboBox comboBox1;
+        private Button button5;
+        private DataGridView dataGridView1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
