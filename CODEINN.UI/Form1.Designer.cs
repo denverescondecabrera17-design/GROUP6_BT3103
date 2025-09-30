d527904cb3c020e2b4f20d6f6d18c05686d30cdf
John Jenry <John Jenry@LAPTOP-6PDRAOVF>
2025-10-01 01:24:44 +0800

    Log in function (no database implementation)

--- CODEINN.UI/Form1.Designer.cs
+++ CODEINN.UI/Form1.Designer.cs
@@ -41,37 +41,41 @@
             // Username
             // 
             Username.Font = new Font("Arial Narrow", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Username.Location = new Point(136, 158);
+            Username.Location = new Point(119, 118);
+            Username.Margin = new Padding(3, 2, 3, 2);
             Username.Name = "Username";
-            Username.Size = new Size(282, 30);
+            Username.Size = new Size(247, 26);
             Username.TabIndex = 0;
             // 
             // Password
             // 
             Password.Font = new Font("Arial Narrow", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Password.Location = new Point(136, 219);
+            Password.Location = new Point(119, 164);
+            Password.Margin = new Padding(3, 2, 3, 2);
             Password.Name = "Password";
-            Password.Size = new Size(282, 30);
+            Password.Size = new Size(247, 26);
             Password.TabIndex = 1;
             Password.TextChanged += Password_TextChanged;
             // 
             // Loginbutton
             // 
             Loginbutton.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            Loginbutton.Location = new Point(217, 268);
+            Loginbutton.Location = new Point(190, 201);
+            Loginbutton.Margin = new Padding(3, 2, 3, 2);
             Loginbutton.Name = "Loginbutton";
-            Loginbutton.Size = new Size(94, 29);
+            Loginbutton.Size = new Size(82, 22);
             Loginbutton.TabIndex = 2;
             Loginbutton.Text = "Login";
             Loginbutton.UseVisualStyleBackColor = true;
+            Loginbutton.Click += Loginbutton_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Arial Narrow", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(36, 161);
+            label1.Location = new Point(32, 121);
             label1.Name = "label1";
-            label1.Size = new Size(93, 24);
+            label1.Size = new Size(75, 20);
             label1.TabIndex = 3;
             label1.Text = "Username:";
             label1.Click += label1_Click;
@@ -80,18 +84,19 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Arial Narrow", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(38, 221);
+            label2.Location = new Point(33, 166);
             label2.Name = "label2";
-            label2.Size = new Size(92, 24);
+            label2.Size = new Size(73, 20);
             label2.TabIndex = 4;
             label2.Text = "Password:";
             // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources._541242678_782352804536515_6233556420554188928_n_removebg_preview;
-            pictureBox1.Location = new Point(358, -18);
+            pictureBox1.Location = new Point(313, -14);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(458, 435);
+            pictureBox1.Size = new Size(401, 326);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 5;
             pictureBox1.TabStop = false;
@@ -100,18 +105,18 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Arial", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(162, 73);
+            label3.Location = new Point(142, 55);
             label3.Name = "label3";
-            label3.Size = new Size(214, 44);
+            label3.Size = new Size(172, 35);
             label3.TabIndex = 6;
             label3.Text = "WELCOME";
             label3.Click += label3_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -119,6 +124,7 @@
             Controls.Add(Password);
             Controls.Add(Username);
             Controls.Add(pictureBox1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
--- CODEINN.UI/Form1.cs
+++ CODEINN.UI/Form1.cs
@@ -1,3 +1,5 @@
+using System.Data;
+
 namespace CODEINN.UI
 {
     public partial class Form1 : Form
@@ -20,6 +22,49 @@ namespace CODEINN.UI
         private void label3_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void Loginbutton_Click(object sender, EventArgs e)
+        {
+            String username, user_password;
+
+            username = Username.Text;
+            user_password = Password.Text;
+
+            try
+            {
+                //String querry = "SELECT * FROM Login_new WHERE username = '" + Username.Text + "' AND password = '" + Password.Text + "'";
+                //SqlDataAdapter sda = new SqlDataAdapter(querry, conn);
+
+                DataTable dtable = new DataTable();
+                // sda.Fill(dtable);
+
+                if (dtable.Rows.Count > 0)
+                {
+                    username = Username.Text;
+                    user_password = Password.Text;
+
+                    Dashboard form2 = new Dashboard();
+                    form2.Show();
+                    this.Hide();
+                }
+
+                else
+                {
+                    MessageBox.Show("Invalid login details", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Username.Clear();
+                    Password.Clear();
+                    Username.Focus();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+            finally
+            {
+                // conn.Close();
+            }
         }
     }
 }
